13d7d7c0a0de19e316bf078726d0f4fba2f7f82a
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-17 22:05:36 +0900

    [FE] update : Stub을 이용해서 단위 테스트 진행 학습

--- src/mock/product/test/product_service_no_di.test.js
+++ src/mock/product/test/product_service_no_di.test.js
@@ -10,17 +10,8 @@ describe("ProductService", () => {
     ];
   });
 
-  /** 
-    - ProductClient Class Mock에 위에서 작성한 fetchItems를 연결하는 방법
-    - 실제로 ProductCliten Class 내부에 fetchItems() 역할을 대신하는 것
-    - 위 작업을 하지 않게 되면 혹여 네트워크에 문제가 생길시 테스트 코드에 영향을 미쳐 테스트가 제대로 이루어지지 않음
-    - 그래서 우리가 테스트 하고자 하는 것에 집중이 가능
-    - 즉, 우리가 확인하고 싶어하는 fetchAvailableItems()가 제대로 filter 역할을 하는지 확인 가능
-    - 이것이 '단위 테스트'
-  */
   ProductClient.mockImplementation(() => {
     return {
-      // fetchItems: fetchItems,
       fetchItems,
     };
   });
@@ -29,10 +20,6 @@ describe("ProductService", () => {
 
   beforeEach(() => {
     productService = new ProductService();
-    // 밑에 함수는 Mock을 Clear()어 하는 방법
-    // jest.config에서 clearMocks : false로 할 경우 밑에 코드를 작성해야 함
-    // fetchItems.mockClear();
-    // ProductClient.mockClear();
   });
 
   it("should filter out only available items", async () => {
